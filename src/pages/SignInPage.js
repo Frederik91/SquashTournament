a89abb5b3bad1616ed16d4cc85b2af0bf0e6c408
Frederik Tegnander <FRTE@COWI.Com>
2023-04-02 18:49:10 +0000

    fixed url

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -12,7 +12,7 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import backgroundImage from './assets/images/signin-background.jpg'
+import backgroundImage from './../assets/images/signin-background.jpg'
 
 function Copyright(props) {
   return (
